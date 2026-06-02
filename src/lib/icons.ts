d9524690dc8494e99cc5b53c260925910902a686
Karan <gandhikaran021@gmail.com>
2026-06-02 14:24:04 +0530

    feat: monochrome icon system via simple-icons

---
+++ src/lib/icons.ts
@@ -0,0 +1,28 @@
+import {
+  siLinux,
+  siStmicroelectronics,
+  siBluetooth,
+  siC,
+  siQualcomm,
+  siGit,
+  siGithub,
+  siLinkedin,
+  siTensorflow,
+  siAnthropic,
+  siEspressif,
+} from 'simple-icons';
+import type { SimpleIcon } from 'simple-icons';
+
+export const icons: Record<string, SimpleIcon> = {
+  linux: siLinux,
+  stm32: siStmicroelectronics,
+  bluetooth: siBluetooth,
+  c: siC,
+  qualcomm: siQualcomm,
+  git: siGit,
+  github: siGithub,
+  linkedin: siLinkedin,
+  tensorflow: siTensorflow,
+  anthropic: siAnthropic,
+  espressif: siEspressif,
+};
